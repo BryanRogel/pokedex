5b3dd2073c92bf6518b1e16da1b934901505f703
Bryan Rogel <bryanrogeldev@gmail.com>
2020-07-26 23:22:50 -0600

    Updates

---
+++ precache-manifest.fdbebbf56a751d35f4a8a35a72a73ca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cffab61b60397d219ab4e661daeff25",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c1552e9f1d7a627cfca3",
+    "url": "/pokedex/static/css/main.e5f3aac3.chunk.css"
+  },
+  {
+    "revision": "6aa254a1b7f85d1e2e49",
+    "url": "/pokedex/static/js/2.abbef634.chunk.js"
+  },
+  {
+    "revision": "2e4f31804d72137a9d9b13db1a0e4557",
+    "url": "/pokedex/static/js/2.abbef634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1552e9f1d7a627cfca3",
+    "url": "/pokedex/static/js/main.3ea51611.chunk.js"
+  },
+  {
+    "revision": "3720d53d7f3565cf8d9c",
+    "url": "/pokedex/static/js/runtime-main.115cfb4e.js"
+  },
+  {
+    "revision": "67d3cdb85f20f089ac72b62636880456",
+    "url": "/pokedex/static/media/loading.67d3cdb8.svg"
+  }
+]);
